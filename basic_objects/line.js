b6b8a3a2f2dd51fe099207ff1f66aab844a779c3
jxm-math <juanjomadrigal326@gmail.com>
2019-04-13 00:31:23 +0200

    Feat: SemiLine

--- basic_objects/line.js
+++ basic_objects/line.js
@@ -68,7 +68,43 @@ class LinePP extends Line{
     }
 }
 
+class SemiLine extends Line{
+    draw() {
+        if (this.v.x == 0 && this.v.y == 0) {
+            this.phys
+                .attr("x1", this.wp.x(this.p.x))
+                .attr("x2", this.wp.x(this.p.x))
+                .attr("y1", this.wp.y(this.p.y))
+                .attr("y2", this.wp.y(this.p.y));
+        } else if (this.v.x != 0) {
+            const slope = this.v.y/this.v.x;
+            const limit = (this.wp.width/this.wp.radius+0.1)*Math.sign(this.v.x);
+            this.phys
+                .attr("x1", this.wp.x(this.p.x))
+                .attr("x2", this.wp.x(limit))
+                .attr("y1", this.wp.y(this.p.y))
+                .attr("y2", this.wp.y(this.p.y+slope*(limit-this.p.x)));
+        } else {
+            const limit = (this.wp.height/this.wp.radius+0.1)*Math.sign(this.v.y);
+            this.phys
+                .attr("x1", this.wp.x(this.p.x))
+                .attr("x2", this.wp.x(this.p.x))
+                .attr("y1", this.wp.y(this.p.y))
+                .attr("y2", this.wp.y(limit));
+        }
+    }
+}
+
+class SemiLinePP extends SemiLine{
+    constructor (p, q) {
+        super(p, new VectorPP(p,q)); 
+        this.q = q;
+    }
+}
+
 export{
     Line,
-    LinePP
+    LinePP,
+    SemiLine,
+    SemiLinePP
 }
