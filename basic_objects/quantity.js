53ab02c594ee1c66fcb83b2726b050e2fa3c7505
jxm-math <juanjomadrigal326@gmail.com>
2019-05-19 11:54:00 +0200

    Feat: Slider

--- basic_objects/quantity.js
+++ basic_objects/quantity.js
@@ -1,4 +1,7 @@
 import {Element} from "./element.js";
+import { Point, DPointOnSegment, FPoint } from "./point.js";
+import { PolyLine } from "./polyline.js";
+import { Text } from "./text.js";
 
 /** Quantity */
 class Quantity extends Element {
@@ -11,9 +14,6 @@ class Quantity extends Element {
         return new Quantity(v);
     }
 
-    insertInto(wp, attrs) {
-        throw new Error('Quantities cannot be represented in a SVG');
-    }
 }
 
 /** Functional Quantity */
@@ -25,6 +25,10 @@ class FQuantity extends Quantity {
         for (let e of array) {
             e.dependents.push(this);
         }
+    }
+
+    insertInto(wp, attrs) {
+        throw new Error('FQuantities cannot be represented in a SVG');
     }
 
     update() {
@@ -42,8 +46,69 @@ class Time extends Quantity {
         }, lapse);
     }
 
+    insertInto(wp, attrs) {
+        throw new Error('Time Quantities cannot be represented in a SVG');
+    }
+
     clear() {clearInterval(this.sInt);}
 
 }
 
-export{Quantity, FQuantity, Time}
+var lineFunction = (wp) => d3.line()
+    .x(function(d) { return wp.x(d.x); })
+    .y(function(d) { return wp.y(d.y); });
+
+class Slider extends Quantity {
+    constructor (a, b, x, y, w, init) {
+        let v = init;
+        if (v<a) v=a;
+        if (v>b) v=b;
+        super(v);
+        this.a = a;
+        this.b = b;
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.default_attrs = {
+            "stroke-width": 2, 
+            "stroke": "black", 
+            "fill": "black",
+            "font-size": "18px"
+        };
+    }
+
+    insertInto(wp, attrs={}) {
+        const [x,y,w] = [this.x,this.y,this.w];
+        this.phys = wp.svg.append("g");
+        this.segment = new PolyLine([new Point(x-w/2,y),new Point(x+w/2,y)]);
+        const t = (this.v-this.a)/(this.b-this.a);
+        const px = x-w/2 + t*w;
+        this.anchor = new DPointOnSegment(px,y,new Point(x-w/2,y),new Point(x+w/2,y));
+
+        this.segment.insertInto(wp, {
+            "stroke-width": attrs["stroke-width"] != undefined ? attrs["stroke-width"] : this.default_attrs["stroke-width"], 
+            "stroke": attrs["stroke"] != undefined ? attrs["stroke"] : this.default_attrs["stroke"], });
+        this.anchor.insertInto(wp, {"fill": attrs["fill"] != undefined ? attrs["fill"] : this.default_attrs["fill"],});
+
+        this.second_anchor = new FPoint(a => {return {x:a.x, y:a.y+0.2}},[this.anchor]);
+        this.text = new Text(this.second_anchor,this);
+        this.text.insertInto(wp, {
+            "font-size": attrs["font-size"] != undefined ? attrs["font-size"] : this.default_attrs["font-size"],});
+
+        this.anchor.dependents.push(this);
+
+        super.insertInto(wp, attrs);
+    }
+
+    update() {
+        this.v = this.a+(this.anchor.x-this.x+this.w/2)*(this.b-this.a)/this.w;
+    }
+
+}
+
+export{
+    Quantity, 
+    FQuantity, 
+    Time,
+    Slider
+}
