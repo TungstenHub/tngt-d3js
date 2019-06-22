b5d45c45ef6f5a04e8fc91634d03d250a2a7b5f3
jxm-math <juanjomadrigal326@gmail.com>
2019-06-22 17:02:53 +0200

    Feat: Clifford's Circle Theorem

---
+++ scripts/clifford_theorem.js
@@ -0,0 +1,56 @@
+import {WorkPlane} from "../utils/init_canvas.js";
+
+import {DPoint, FPoint} from "../basic_objects/point.js";
+import {Circle3P, CirclePP} from "../basic_objects/circle.js";
+
+import {mdColor as color} from "../utils/material_color.js";
+
+const
+wp = WorkPlane.with("#clifford_theorem", 100),
+
+o = new DPoint(-0.5,0),
+
+a = new DPoint(-1,-1),
+b = new DPoint(-1, 1),
+c = new DPoint( 1,-1),
+d = new DPoint( 1, 1),
+
+int_pair = (P,Q,O) => {
+    const a = P.x-O.x;
+    const b = P.y-O.y;
+    const c = Q.x-O.x;
+    const d = Q.y-O.y;
+    const t = 2*(a*d-b*c)/((a-c)*(a-c)+(b-d)*(b-d));
+    return {x: O.x + t*(d-b), y: O.y + t*(a-c)}
+},
+
+iab = new FPoint(int_pair, [a,b,o]),
+iac = new FPoint(int_pair, [a,c,o]),
+iad = new FPoint(int_pair, [a,d,o]),
+ibc = new FPoint(int_pair, [b,c,o]),
+ibd = new FPoint(int_pair, [b,d,o]),
+icd = new FPoint(int_pair, [c,d,o]),
+
+c_a = new CirclePP(a,o),
+c_b = new CirclePP(b,o),
+c_c = new CirclePP(c,o),
+c_d = new CirclePP(d,o),
+
+c_abc = new Circle3P(iab,iac,ibc),
+c_abd = new Circle3P(iab,iad,ibd),
+c_acd = new Circle3P(iac,iad,icd),
+c_bcd = new Circle3P(ibc,ibd,icd),
+
+o_abc = c_abc.center(),
+o_abd = c_abd.center(),
+
+m = new FPoint(int_pair, [o_abc,o_abd,iab]);
+
+wp.append([c_a,c_b,c_c,c_d], {"stroke": color.blue.w800});
+wp.append([c_abc,c_abd,c_acd,c_bcd], {"stroke": color.blue.w200});
+wp.append(o, {"fill": 'white'});
+wp.append(m, {"fill": color.pink.w800,"stroke": 'white','stroke-width': 2});
+wp.append([iab,iac,iad,ibc,ibd,icd], {"fill": color.blue.w500});
+wp.append([a,b,c,d], {"fill": color.blue.w800});
+
+wp.end();
